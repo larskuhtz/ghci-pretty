129dca41262095df64ba6fef011a9327d09fd4f0
Lars Kuhtz <lars@alephcloud.com>
2014-11-13 13:02:25 -0800

    improve documentation

--- src/IPPrint/Colored.hs
+++ src/IPPrint/Colored.hs
@@ -1,7 +1,32 @@
--- ------------------------------------------------------ --
--- Copyright © 2014 Lars Kuhtz <lakuhtz@gmail.com>
--- ------------------------------------------------------ --
-
+-- |
+-- Module: IPPrint.Colored
+-- Copyright: Copyright © 2014 Lars Kuhtz <lakuhtz@gmail.com>
+-- License: MIT
+-- Maintainer: Lars Kuhtz <lakuhtz@gmail.com>
+-- Stability: experimental
+--
+-- This module combines the ipprint package and
+-- the hscolour package to provide colored pretty-printing
+-- in ghci.
+--
+-- /Usage/
+--
+-- Add the following lines to your @ghci.conf@ file:
+--
+-- > -- Pretty printing of it
+-- > import IPPrint.Colored
+-- > :set -interactive-print=IPPrint.Colored.cpprint
+-- > :def cp (\_ -> return ":set -interactive-print=IPPrint.Colored.cpprint")
+-- > :def ncp (\_ -> return ":set -interactive-print=print")
+--
+-- Now you can enable colored pretty-printing in ghci with the commmand
+--
+-- > :cp
+--
+-- The following command turns colored pretty-printing off again
+--
+-- > :ncp
+--
 module IPPrint.Colored
 ( cpprint
 ) where
